524116b688506406c33cb427265f980b684ab7dc
Marina Schramm Jorge Schneider <84358232+marinasjs@users.noreply.github.com>
2022-05-09 16:45:08 -0300

    Add files via upload

---
+++ CalculadorPontuacao.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alura\Solid\Service;
+
+use Alura\Solid\Model\AluraMais;
+use Alura\Solid\Model\Curso;
+
+class CalculadorPontuacao
+{
+    public function recuperarPontuacao(Pontuavel $conteudo)
+    {
+       return $conteudo->recuperarPontuacao();
+    }
+}
